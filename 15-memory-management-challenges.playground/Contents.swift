589cb6050c4e6239f8d3fb88bd160b22fdc4fdfc
Craftcode <user195223@74-80-245-101.ic2net.net>
2021-03-29 12:16:47 -0700

    Ex. #15

--- 15-memory-management-challenges.playground/Contents.swift
+++ 15-memory-management-challenges.playground/Contents.swift
@@ -66,9 +66,42 @@
  car = nil
  ```
  */
+class Person {
+    let name: String
+    let email: String
+    weak var car: Car?
+        
+    init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
+        
+    deinit {
+        print("Goodbye \(name)!")
+    }
+}
 
-// your code here
+class Car {
+    let id: Int
+    let type: String
+    var owner: Person?
+        
+    init(id: Int, type: String) {
+        self.id = id
+        self.type = type
+    }
+    
+    deinit {
+        print("Goodbye \(type)!")
+    }
+}
 
+var owner: Person? = Person(name: "Cosmin", email: "cosmin@whatever.com")
+var car: Car? = Car(id: 10, type: "BMW")
+owner?.car = car
+car?.owner = owner
+owner = nil
+car = nil
 /*:
  ### Challenge 2
  
@@ -115,5 +148,41 @@
  customer = nil
  ```
  */
+class Customer {
+    let name: String
+    let email: String
+    weak var account: Account?
 
-// your code here
+    init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
+
+    deinit {
+        print("Goodbye \(name)!")
+    }
+}
+
+class Account {
+    let number: Int
+    let type: String
+    let customer: Customer
+
+    init(number: Int, type: String, customer: Customer) {
+        self.number = number
+        self.type = type
+        self.customer = customer
+    }
+
+    deinit {
+        print("Goodbye \(type) account number \(number)!")
+    }
+}
+
+var customer: Customer? = Customer(name: "George", email: "george@whatever.com")
+var account: Account? = Account(number: 10, type: "PayPal", customer: customer!)
+
+customer?.account = account
+
+account = nil
+customer = nil
